bce51dbfaba39e9ad95a47afd7149da92d538085
Pablo Estrada <pablof300@gmail.com>
2021-01-27 14:36:47 -0500

    Refactored to functional components

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,27 +2,27 @@ import React from "react";
 import { Card, Image, Icon } from "semantic-ui-react";
 import styles from "../assets/styles.module.css";
 
-export default class Movie extends React.Component {
-  render() {
-    return (
-      <Card>
-        {/* Task: use movie props to render dynamic information */}
-        <Image
-          src="https://www.gsmr.com/sites/default/files/imagecache/event_image/01KEYARTBU18169_KA_PE%20Resized.png"
-          size="medium"
-        />
-        <Card.Content>
-          <Card.Header>Polar express</Card.Header>
-          <Card.Meta>2004</Card.Meta>
-          <Card.Description>best movie ever!!!!</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <a>
-            <Icon name="user" />
-            9.5/10
-          </a>
-        </Card.Content>
-      </Card>
-    );
-  }
+const Movie = (props) => {
+  return (
+    <Card>
+      {/* Task: use movie props to render dynamic information */}
+      <Image
+        src="https://www.gsmr.com/sites/default/files/imagecache/event_image/01KEYARTBU18169_KA_PE%20Resized.png"
+        size="medium"
+      />
+      <Card.Content>
+        <Card.Header>Polar express</Card.Header>
+        <Card.Meta>2004</Card.Meta>
+        <Card.Description>best movie ever!!!!</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="user" />
+          9.5/10
+        </a>
+      </Card.Content>
+    </Card>
+  );
 }
+
+export default Movie
--- src/components/MovieScreen.js
+++ src/components/MovieScreen.js
@@ -12,45 +12,42 @@ import {
 import styles from "../assets/styles.module.css";
 import Movie from "./Movie";
 
-export default class MovieScreen extends React.Component {
-  // Task: Add a constructor to initialize empty state
-  constructor(props) {
-      super(props)
-  }
-
+const MovieScreen = () => {
   // Task: update state to add new movie (called when pressing submit on form)
-  submitNewMovie = () => {
+  // hint: https://stackoverflow.com/questions/54676966/push-method-in-react-hooks-usestate
+  const submitNewMovie = () => {
   };
 
-  render() {
-    return (
-      <Card raised className={styles.movieScreen}>
-        <Card.Content>
-          <Card fluid>
-            <Card.Header>
-              {/* Task: Add some padding to the header */}
-              <h2>My Holiday Movies</h2>
-            </Card.Header>
-            <Card.Content>
-              <Card.Group>
-                {/* Render your holiday movies using the movie component and state here (hint: use map)*/}
-              </Card.Group>
-            </Card.Content>
-          </Card>
-          <Divider></Divider>
-          <Card fluid>
-            <Card.Header>
-              {/* Task: Add some padding to the header */}
-              <h2>Add new movie</h2>
-            </Card.Header>
-            <Card.Content>
-              {/* Task: Add a form with the five Form.Field (movie name, movie image link, movie year, personal comment, personal rating) and submit button */}
-              <Form>
-              </Form>
-            </Card.Content>
-          </Card>
-        </Card.Content>
-      </Card>
-    );
-  }
+  return (
+    <Card raised className={styles.movieScreen}>
+      <Card.Content>
+        <Card fluid>
+          <Card.Header>
+            {/* Task: Add some padding to the header */}
+            <h2>My Holiday Movies</h2>
+          </Card.Header>
+          <Card.Content>
+            <Card.Group>
+              {/* Render your holiday movies using the movie component and state here (hint: use map)*/}
+              {/* {Hint: use .map() */}
+            </Card.Group>
+          </Card.Content>
+        </Card>
+        <Divider></Divider>
+        <Card fluid>
+          <Card.Header>
+            {/* Task: Add some padding to the header */}
+            <h2>Add new movie</h2>
+          </Card.Header>
+          <Card.Content>
+            {/* Task: Add a form with the five Form.Field (movie name, movie image link, movie year, personal comment, personal rating) and submit button */}
+            <Form>
+            </Form>
+          </Card.Content>
+        </Card>
+      </Card.Content>
+    </Card>
+  );
 }
+
+export default MovieScreen
